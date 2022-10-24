227d542501d535fd9feac1be58be495740eb8c60
Ziedelth <watteaupierre@gmail.com>
2022-10-24 18:16:20 +0200

    Add diary

---
+++ src/main/kotlin/fr/ziedelth/controllers/DiaryController.kt
@@ -0,0 +1,35 @@
+package fr.ziedelth.controllers
+
+import fr.ziedelth.entities.Anime
+import fr.ziedelth.utils.Database
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+object DiaryController : IController<Anime>("/diary") {
+    fun Routing.getDiary() {
+        route(prefix) {
+            get("/country/{country}") {
+                val country = call.parameters["country"] ?: return@get call.respond(HttpStatusCode.BadRequest)
+                println("GET $prefix/country/$country")
+                val session = Database.getSession()
+
+                try {
+                    val query = session.createQuery(
+                        "SELECT anime FROM Episode episode WHERE episode.anime.country.tag = :tag AND current_date - to_date(episode.releaseDate, 'YYYY-MM-DDTHH:MI:SS') <= 7 ORDER BY episode.releaseDate DESC",
+                        entityClass
+                    )
+                    query.setParameter("tag", country)
+                    val list = query.list()?.distinctBy { it.uuid }
+                    call.respond(list ?: HttpStatusCode.NotFound)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    call.respond(HttpStatusCode.InternalServerError, e.message ?: "Unknown error")
+                } finally {
+                    session.close()
+                }
+            }
+        }
+    }
+}
