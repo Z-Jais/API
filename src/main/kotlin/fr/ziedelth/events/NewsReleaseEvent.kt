f9d386ec68fac9266988bfb785fedf464e57ae4e
Ziedelth <watteaupierre@gmail.com>
2022-10-25 20:37:36 +0200

    fix

--- src/main/kotlin/fr/ziedelth/events/NewsReleaseEvent.kt
+++ src/main/kotlin/fr/ziedelth/events/NewsReleaseEvent.kt
@@ -3,4 +3,4 @@ package fr.ziedelth.events
 import fr.ziedelth.entities.News
 import fr.ziedelth.utils.plugins.events.Event
 
-data class NewsReleaseEvent(val mangas: Collection<News>) : Event
+data class NewsReleaseEvent(val news: Collection<News>) : Event
