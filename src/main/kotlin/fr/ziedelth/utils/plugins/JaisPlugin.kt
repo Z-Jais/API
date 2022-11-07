70c0d90ac28aa58aa5c40fb673419e4ae72076aa
Ziedelth <watteaupierre@gmail.com>
2022-11-07 14:59:23 +0100

    Add data folder for plugins

--- src/main/kotlin/fr/ziedelth/utils/plugins/JaisPlugin.kt
+++ src/main/kotlin/fr/ziedelth/utils/plugins/JaisPlugin.kt
@@ -2,5 +2,17 @@ package fr.ziedelth.utils.plugins
 
 import org.pf4j.Plugin
 import org.pf4j.PluginWrapper
+import java.io.File
 
-abstract class JaisPlugin(pluginWrapper: PluginWrapper) : Plugin(pluginWrapper)
+abstract class JaisPlugin(pluginWrapper: PluginWrapper) : Plugin(pluginWrapper) {
+    private val pluginsFolder = File("plugins")
+        get() {
+            if (!field.exists()) {
+                field.mkdirs()
+            }
+
+            return field
+        }
+
+    val dataFolder = File(pluginsFolder, pluginWrapper.pluginId)
+}
