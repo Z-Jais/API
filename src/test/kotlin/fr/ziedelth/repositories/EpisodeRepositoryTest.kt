bb4b6b9148e6f4a1f258548683378770322f7d26
Ziedelth <watteaupierre@gmail.com>
2022-11-20 18:56:47 +0100

    Add more tests for EpisodeRepository

--- src/test/kotlin/fr/ziedelth/repositories/EpisodeRepositoryTest.kt
+++ src/test/kotlin/fr/ziedelth/repositories/EpisodeRepositoryTest.kt
@@ -1,6 +1,7 @@
 package fr.ziedelth.repositories
 
 import fr.ziedelth.AbstractAPITest
+import fr.ziedelth.plugins.animeRepository
 import fr.ziedelth.plugins.episodeRepository
 import org.junit.jupiter.api.Test
 import kotlin.test.expect
@@ -11,6 +12,26 @@ internal class EpisodeRepositoryTest : AbstractAPITest() {
         val page1 = episodeRepository.getByPage("fr", 1, 2)
         expect(2) { page1.size }
         val page2 = episodeRepository.getByPage("fr", 2, 2)
+        expect(2) { page2.size }
+    }
+
+    @Test
+    fun getByPageWithAnime() {
+        val anime = animeRepository.getAll().first()
+
+        val page1 = episodeRepository.getByPageWithAnime(anime.uuid, 1, 2)
+        expect(2) { page1.size }
+        val page2 = episodeRepository.getByPageWithAnime(anime.uuid, 2, 2)
+        expect(2) { page2.size }
+    }
+
+    @Test
+    fun getByPageWithList() {
+        val animes = animeRepository.getAll().take(2).map { it.uuid }
+
+        val page1 = episodeRepository.getByPageWithList(animes, 1, 2)
+        expect(2) { page1.size }
+        val page2 = episodeRepository.getByPageWithList(animes, 2, 2)
         expect(2) { page2.size }
     }
 }
